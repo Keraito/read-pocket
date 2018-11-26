4b6f8433a873c88f0f170071f5d5340cdbcbc7d1
Keraito <dragoon_v2_chak_shun@hotmail.com>
2018-11-26 16:16:29 +0100

    Add requests of access token and articles

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import request from 'request';
+import open from 'open';
 
 const headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
@@ -7,6 +8,9 @@ const headers = {
 
 const urls = {
   request: 'https://getpocket.com/v3/oauth/request',
+  authorize: 'https://getpocket.com/v3/oauth/authorize',
+  get: 'https://getpocket.com/v3/get',
+  redirect: 'https://github.com',
 };
 
 export const obtainRequestToken = consumerKey =>
@@ -14,7 +18,29 @@ export const obtainRequestToken = consumerKey =>
     const options = {
       headers,
       url: urls.request,
-      body: `consumer_key=${consumerKey}&redirect_uri=pocketapp1234:authorizationFinished`,
+      body: `consumer_key=${consumerKey}&redirect_uri=${urls.redirect}`,
+    };
+    request.post(options, (err, resp, body) => {
+      if (err) rej(err);
+      const result = JSON.parse(body);
+      redirect(result.code);
+      res(result);
+    });
+  });
+
+const redirect = requestToken =>
+  open(
+    `https://getpocket.com/auth/authorize?request_token=${requestToken}&redirect_uri=${
+      urls.redirect
+    }`
+  );
+
+export const obtainAccessToken = (consumerKey, requestToken) =>
+  new Promise((res, rej) => {
+    const options = {
+      headers,
+      url: urls.authorize,
+      body: `consumer_key=${consumerKey}&code=${requestToken}`,
     };
     request.post(options, (err, resp, body) => {
       if (err) rej(err);
@@ -22,4 +48,15 @@ export const obtainRequestToken = consumerKey =>
     });
   });
 
-export const obtainAccessToken = () => undefined;
+export const getArticles = (consumerKey, accessToken) =>
+  new Promise((res, rej) => {
+    const options = {
+      headers,
+      url: urls.get,
+      body: `consumer_key=${consumerKey}&access_token=${accessToken}&count=1&contentType=article`,
+    };
+    request.post(options, (err, resp, body) => {
+      if (err) rej(err);
+      res(JSON.parse(body));
+    });
+  });
--- index.test.js
+++ index.test.js
@@ -1,15 +1,28 @@
-import { obtainRequestToken, obtainAccessToken } from '.';
+import { obtainRequestToken, obtainAccessToken, getArticles } from '.';
 
 describe('The Pocket API', () => {
   const consumerKey = '82175-555ac50456965486c86eeb53';
-  test('should correctly obtain the request token.', () => {
-    const result = obtainRequestToken(consumerKey);
-    return expect(result).resolves.toHaveProperty('code');
+  let requestToken;
+  let access_token;
+
+  test('should correctly obtain the request token.', async () => {
+    const result = await obtainRequestToken(consumerKey);
+    expect(result).toHaveProperty('code');
+    requestToken = result.code;
+  }, 10000);
+
+  test('should correctly convert a request token into a Pocket access token.', async () => {
+    expect(requestToken).toBeTruthy();
+    const result2 = await obtainAccessToken(consumerKey, requestToken);
+    expect(result2).toHaveProperty('access_token');
+    expect(result2).toHaveProperty('username');
+    access_token = result2.access_token;
   });
 
-  // test('should correctly convert a request token into a Pocket access token.', () => {
-  //   const result = obtainAccessToken();
-  //   expect(result).toHaveProperty('access_token');
-  //   expect(result).toHaveProperty('username');
-  // });
+  test('should correctly retrieve an article', async () => {
+    expect(access_token).toBeTruthy();
+    const result3 = await getArticles(consumerKey, access_token);
+    expect(result3.status).toBeTruthy();
+    expect(Object.keys(result3.list).length).toBeGreaterThan(0);
+  }, 10000);
 });
